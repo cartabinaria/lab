85cf42d826bf544e6c846e0c6b73156df7bdd8b6
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-11-03 00:12:12 +0100

    Finalize sh

--- 01-introduzione-a-git-comandi.sh
+++ 01-introduzione-a-git-comandi.sh
@@ -4,6 +4,7 @@ ssh stefano.volpe2@mingo.cs.unibo.it
 # Ambiente di lavoro | Preparazione (3)
 git config --global user.name "Stefano Volpe"
 git config --global user.email "stefano.volpe2@studio.unibo.it"
+ssh-keygen -t ed25519 -C "stefano.volpe2@studio.unibo.it"
 
 # git init
 git init hello-git
@@ -14,6 +15,13 @@ ls -a
 # git status
 git status
 vi hello-world.c
+git status
+
+# .gitignore
+gcc hello-world.c
+ls
+git status
+vi .gitignore
 git status
 
 # git add
@@ -49,3 +57,13 @@ git show
 # git branch, checkout
 git branch
 git checkout -b testing
+git branch
+vi hello-world.c
+git add .
+git status
+git commit -m "Some nasty testing"
+git checkout -b main
+vi hello-world.c
+git add .
+git status
+git commit -m "Serious business"
